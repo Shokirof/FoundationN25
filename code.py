bd222273d316a59d06101a5cb5d35f3b74e1c9e1
ShohsanamAbdujabborova <138601744+ShohsanamAbdujabborova@users.noreply.github.com>
2023-07-05 03:24:54 -0400

    Add files via upload

--- code.py
+++ code.py
@@ -1,19 +1,2 @@
-from os import system
-system("cls")
-def summa(a,b):
-    return a+b
-def reverse(a):
-    while (a):
-        print(a%10)
-        a//=10
-print(reverse(125478))
-def shavkat(a,b):
-    if a:
-        print("Hello")
-    if b:
-        print("Salom")
-    else:
-        print("None")
+something
 
-def show_info(self):
-    
