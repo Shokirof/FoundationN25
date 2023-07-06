8d92b6451bc5eaff9dd31e46c0015b58426ce1a5
Ne'matova <nematovahusnidabonu11@gmail.com>
2023-07-06 13:29:43 +0500

    vazifaa

--- Qiziqma.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout<<"Git hub bilan ishlash qiziqarli lekin sizlarga farqi yuq"<<endl;
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# FoundationN25
-Bu loyiga FOundation N25 o'quvchilari uchun yaratilgan
